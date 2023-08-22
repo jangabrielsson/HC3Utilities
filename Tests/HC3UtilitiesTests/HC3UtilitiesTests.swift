36a1e1d33987d707733783a50b965927d9d73bc0
Jan Gabrielsson <jan@gabrielsson.com>
2023-08-22 17:26:29 +0200

    initial

--- Tests/HC3UtilitiesTests/HC3UtilitiesTests.swift
+++ Tests/HC3UtilitiesTests/HC3UtilitiesTests.swift
@@ -2,10 +2,17 @@ import XCTest
 @testable import HC3Utilities
 
 final class HC3UtilitiesTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HC3Utilities().text, "Hello, World!")
+    var hc3: HC3Utilities = HC3Utilities(ip:"192.168.1.57",user:"admin",password:"admin")
+    
+    func testDecodeJSON() throws {
+        let data = stringFrom(resource:"test1.device")!
+        if let res = try? hc3.decode(str:data) as HC3Utilities.types.Device {
+            XCTAssertEqual(res.name, "Dimmer switch")
+        }
+    }
+    
+    func testDetDevices() throws {
+        let res = try hc3.fibaro.getDevices()
+        XCTAssert(res.count > 0)
     }
 }
