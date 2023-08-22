37310d453e7ce8895b819f99d88fe6225f6ec83c
Jan Gabrielsson <jan@gabrielsson.com>
2023-08-22 17:26:53 +0200

    initial

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "HC3Utilities",
+    platforms: [
+            .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +23,17 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HC3Utilities",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+//                .copy("device1")
+            ]
+        ),
         .testTarget(
             name: "HC3UtilitiesTests",
-            dependencies: ["HC3Utilities"]),
+            dependencies: ["HC3Utilities"],
+            resources: [
+                .copy("Resources/test1.device")
+            ]
+            ),
     ]
 )
